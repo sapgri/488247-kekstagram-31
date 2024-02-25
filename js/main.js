2b59b23a29e4769d62a703d67490cdc7398c516e
SapGri <sapgri360@gmail.com>
2024-02-25 09:33:27 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -64,8 +64,8 @@ const Comments = {
 const photos = [];
 
 const getRandomInteger = (min, max) => {
-  const lower = Math.floor(Math.min(min, max));
-  const upper = Math.ceil(Math.max(min, max));
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
